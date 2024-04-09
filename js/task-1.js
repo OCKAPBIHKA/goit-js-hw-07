f70aa4a0351059840b6b84bc0a17c17fb4adaa95
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-04-09 21:07:52 +0300

    worck from task 1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const categoriesList = document.getElementById('categories');
+
+const categoryItems = categoriesList.querySelectorAll('li.item');
+
+console.log('Number of categories: ' + categoryItems.length);
+
+categoryItems.forEach(item => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const categoryElements = item.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElements}`);
+});
--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,4 @@
+'use strict';
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -10,5 +11,5 @@ const images = [
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
-  }
+  },
 ];
