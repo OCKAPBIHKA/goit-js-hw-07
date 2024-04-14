c661b4101358b06a3ecc1689340fbf35f8ccb016
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-04-14 13:15:08 +0300

    make task 5-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,6 +1,42 @@
 'use strict';
+
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('#controls button[data-create]');
+const destroyBtn = document.querySelector('#controls button[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value);
+
+  if (isNaN(amount) || amount < 1 || amount > 100) {
+    return;
+  }
+
+  destroyBoxes();
+  createBoxes(amount);
+  input.value = '';
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
