768a29efa60fa933dd33917db495a3518e73da50
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-04-14 10:44:49 +0300

    make task 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,12 @@
 'use strict';
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', () => {
+  const nameValue = nameInput.value.trim();
+  if (nameValue === '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = nameValue;
+  }
+});
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
 'use strict';
+const logForm = document.querySelector('.login-form');
+
+logForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailInput = document.querySelector('input[name="email"]');
+  const passwordInput = document.querySelector('input[name="password"]');
+
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (!email || !password) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  logForm.reset();
+});
